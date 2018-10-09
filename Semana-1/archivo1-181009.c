c5924b175798b5fd4c2cee38d06fa9e4edb4d2d9
Cesar Alexis Velazquez Marin <cesalexmv17@gmail.com>
2018-10-09 14:18:03 -0700

    Primer programa

---
+++ Semana-1/archivo1-181009.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main(){
+
+int Numero=1;
+
+printf("Hola mundo\n");
+
+printf("\tHola cesar");
+
+printf("\nMi numero es %d",Numero);
+
+Numero=3;
+
+printf("\n Mi numero es %d",Numero);
+
+}
+
