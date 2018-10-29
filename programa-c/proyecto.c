ed84f2e765fd1dfa1769aaceeadce63f01b0c776
Cesar Alexis Velazquez Marin <cesalexmv17@gmail.com>
2018-10-29 17:32:10 -0600

    Hola

--- programa-c/proyecto.c
+++ programa-c/proyecto.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-
 int main(){
-
 	char ma[4][13]={{'q','w','e','r','t','y','u','i','o','p','a','s','d'},{'f','g','h','j','k','l','z','x','c','v','b','n','m'},{'n','m','b','v','c','x','z','l','k','j','h','g','f'},{'d','s','a','p','o','i','u','y','t','r','e','w','q'}};
 	int j,i,k,m=1,a,b,c,d,intentos=1;
 	printf("**********************************************\n");
@@ -26,11 +24,48 @@ int main(){
 			printf("Digite la cordenada de la segunda letra: ");
 			scanf("%i,%i",&c,&d);
 
-			if(a==3 && b==4 && c==11 && d==1){
-				printf("Acertaste la cordenada");	
+			if(a==3 && b==4 && c==11 && d==1 || a==11 && b==1 && c==3 && d==4 ){
+				printf("Acertaste la cordenada de la letra a");	
 			}
-			else if(a==3 && b==3 && c==11 && d==2){
-				printf("Acertaste la cordenada");
+			else if(a==3 && b==3 && c==11 && d==2 || a==11 && b==2 && c==3 && d==3){
+				printf("Acertaste la cordenada de la letra b");
 			}
+			else if(a==5 && b==3 && c==9 && d==2 || a==9 && b==2 && c==5 && b==3){
+				printf("Acertaste la cordenada de la letra c");
+			}
+			else if(a==5 && b==3 && c==9 && d==2 || a==9 && b==2 && c==5 && d==3){
+                                printf("Acertaste la cordenada de la letra d");
+                        }
+			else if(a==3 && b==1 && c==11 && d==4 || a==11 && b==4 && c==3 && d==1){
+                                printf("Acertaste la cordenada de la letra e");
+                        }
+                        else if(a==1 && b==2 && c==13 && d==3 || a==13 && b==3 && c==1 && b==2){                  
+                                printf("Acertaste la cordenada de la letra f");
+                        }
+                        else if(a==12 && b==3 && c==2 && d==2 || a==2 && b==2 && c==13 && d==3){
+                                printf("Acertaste la cordenada de la letra g");
+                        }
+			else if(a==3 && b==2 && c==11 && d==3 || a==11 && b==3 && c==3 && d==2){
+                                printf("Acertaste la cordenada de la letra h");
+                        }
+                        else if(a==8 && b==1 && c==6 && d==4 || a==6 && b==4 && c==8 && b==1){                  
+                                printf("Acertaste la cordenada de la letra i");
+                        }
+			else if(a==4 && b==2 && c==10 && d==3 || a==10 && b==3 && c==4 && d==2){
+                                printf("Acertaste la cordenada de la letra j");
+                        }
+                        else if(a==9 && b==3 && c==5 && d==2 || a==5 && b==2 && c==9 && b==3){                  
+                                printf("Acertaste la cordenada de la letra k");
+                        }
+                        else if(a==8 && b==3 && c==6 && d==2 || a==6 && b==2 && c==8 && d==3){
+                                printf("Acertaste la cordenada de la letra l");
+                        }
+			else if(a==2 && b==3 && c==13 && d==2 || a==13 && b==2 && d==2 && d==3){
+				printf("Acertaste la cordenada de la letra m");
+			}
+			else{
+				 printf("Error las cordenadas digitadas no corresponden a ningun par de letras");
+			}
+
 		}
 }	
